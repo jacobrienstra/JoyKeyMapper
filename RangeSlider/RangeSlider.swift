fc1b1859cff6ec409401a029f19127f56fe023df
Jacob Rienstra <jacobdrienstra@gmail.com>
2020-08-19 03:57:37 -0400

    rangeslider works??????

--- RangeSlider/RangeSlider.swift
+++ RangeSlider/RangeSlider.swift
@@ -21,8 +21,8 @@ import Cocoa
         super.draw(dirtyRect)
     
         let path = NSBezierPath(ovalIn: dirtyRect.insetBy(dx: 1.0, dy: 1.0))
-        let fillColor = NSColor.white
-        let borderColor = NSColor(red: 177, green: 177, blue: 177, alpha: 1)
+        let fillColor = NSColor(red: 205/255, green: 205/255, blue: 205/255, alpha: 1)
+        let borderColor = NSColor(red: 177/255, green: 177/255, blue: 177/255, alpha: 1)
         fillColor.setFill()
         path.fill()
         path.lineWidth = 0.25
@@ -59,6 +59,12 @@ public protocol RangeSliderDelegate {
     // func controller(_ controller: RangeSlider, willBeginChangeValues first:CGFloat, second:CGFloat)
 }
 
+extension CGFloat {
+    var clean: String {
+       return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", Float(self)) : String(Float(self))
+    }
+}
+
 @IBDesignable public class RangeSlider: NSControl, CALayerDelegate {
    
     
@@ -78,46 +84,56 @@ public protocol RangeSliderDelegate {
         }
     }
     
-    @IBInspectable public var size = CGSize(width: 18.0, height: 18.0) {
+    @IBInspectable public var size = CGSize(width: 16.5, height: 16.5) {
         didSet {
             updateLayerFrames()
         }
     }
 
-    private var previousLocation = CGPoint()
-    var delegate: RangeSliderDelegate?
-    var shouldMoveFirst: Bool = false
-    var shouldMoveLast: Bool = false
-
-    // TODO: Add option to hide info labels
-    
     @IBInspectable public var min: CGFloat = 0 {
         didSet {
             updateLayerFrames()
         }
     }
-    @IBInspectable public var max: CGFloat = 1 {
+    @IBInspectable public var max: CGFloat = 100 {
         didSet {
             updateLayerFrames()
         }
     }
         
-    @IBInspectable public var lowerValue: CGFloat = 0.2 {
+    @IBInspectable public var lowerValue: CGFloat = 25 {
         didSet {
             updateLayerFrames()
         }
     }
     
-    @IBInspectable public var upperValue: CGFloat = 0.8 {
+    @IBInspectable public var upperValue: CGFloat = 75 {
         didSet {
             updateLayerFrames()
         }
     }
     
+    @IBInspectable public var step: CGFloat = 1.0 {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    private var previousLocation = CGPoint()
+    var delegate: RangeSliderDelegate?
+    var shouldMoveFirst: Bool = false
+    var shouldMoveLast: Bool = false
     private let baseLayer = CALayer()
     private let trackLayer = RangeSliderTrackLayer()
     private let lowerKnob = CustomKnob()
     private let upperKnob = CustomKnob()
+    var firstLabel: NSTextField = NSTextField(frame: NSMakeRect(0, -2, 30, 20))
+    var secondLabel: NSTextField = NSTextField(frame: NSMakeRect(0, -2, 30, 20))
+    let textYOrigin: CGFloat = 0
+    var maxLineWidth: CGFloat {
+        get {
+            return self.bounds.width - size.width
+        }
+    }
     
     override public init(frame: CGRect) {
         super.init(frame: frame)
@@ -140,10 +156,31 @@ public protocol RangeSliderDelegate {
         self.layer = baseLayer
         self.isEnabled = true
         self.isContinuous = true
+        setUpLabels()
         addSubview(lowerKnob)
         addSubview(upperKnob)
+        addSubview(firstLabel)
+        addSubview(secondLabel)
         updateLayerFrames()
         
+    }
+
+    func setUpLabels() {
+        firstLabel.isBordered = false
+        firstLabel.isEditable = false
+        firstLabel.isSelectable = false
+        firstLabel.alignment = NSTextAlignment.center
+        firstLabel.setContentHuggingPriority(.required, for: .horizontal)
+        firstLabel.backgroundColor = .none
+        firstLabel.textColor = textColor
+
+        secondLabel.isBordered = false
+        secondLabel.isEditable = false
+        secondLabel.isSelectable = false
+        secondLabel.alignment = NSTextAlignment.center
+        secondLabel.setContentHuggingPriority(.required, for: .horizontal)
+        secondLabel.backgroundColor = .none
+        secondLabel.textColor = textColor
     }
     
     override public var frame: CGRect {
@@ -161,13 +198,27 @@ public protocol RangeSliderDelegate {
         trackLayer.setNeedsDisplay()
         lowerKnob.frame = CGRect(origin: thumbOriginForValue(lowerValue), size: size)
         upperKnob.frame = CGRect(origin: thumbOriginForValue(upperValue), size: size)
+        firstLabel.stringValue  = "\(lowerValue.clean)"
+        secondLabel.stringValue = "\(upperValue.clean)"
+        firstLabel.setFrameOrigin(NSMakePoint(lowerKnob.frame.midX - firstLabel.frame.width / 2, textYOrigin))
+        secondLabel.setFrameOrigin(NSMakePoint(upperKnob.frame.midX - secondLabel.frame.width / 2, textYOrigin))
+
         CATransaction.commit()
     }
     
     // 2
     func positionForValue(_ value: CGFloat) -> CGFloat {
-        return bounds.width * value
+        return (((value - min) / (max - min)) * (maxLineWidth)) + (size.width / 2)
     }
+    
+    func valueForPosition(_ pos: CGFloat) -> CGFloat {
+        return ((pos - (size.width / 2)) / maxLineWidth) * (max - min) + min
+    }
+    
+    func getSteppedValue(_ value: CGFloat, rule: FloatingPointRoundingRule) -> CGFloat {
+        return (((value - min) / step).rounded(rule) * step) + min
+    }
+
     
     // 3
     private func thumbOriginForValue(_ value: CGFloat) -> CGPoint {
@@ -185,7 +236,6 @@ public protocol RangeSliderDelegate {
     }
 
     
-    /// Mouse down event : We test if current mouse location is inside of first or second knob. If yes, then we
     public override func mouseDown(with theEvent: NSEvent) {
         let loc = self.convert(theEvent.locationInWindow, from: self.window?.contentView)
         previousLocation = loc
@@ -202,181 +252,77 @@ public protocol RangeSliderDelegate {
     public override func mouseDragged(with theEvent: NSEvent) {
         let loc = self.convert(theEvent.locationInWindow, from: self.window?.contentView)
         // 1
-        let deltaLocation = loc.x - previousLocation.x
-        let deltaValue = (max - min) * deltaLocation / bounds.width
+//        let deltaLocation = loc.x - previousLocation.x
+//        let deltaValue = (max - min) * deltaLocation / maxLineWidth
         previousLocation = loc
 
         if shouldMoveFirst {
-            lowerValue += deltaValue
-            lowerValue = boundValue(lowerValue, toLowerValue: min, upperValue: upperValue)
+//            lowerValue += deltaValue
+            let realMaxVal = valueForPosition(upperKnob.frame.minX - size.width / 2)
+//            lowerValue = boundValueStepped(lowerValue, toLowerValue: min, upperValue: realMaxVal)
+            lowerValue = boundValueStepped(valueForPosition(loc.x), toLowerValue: min, upperValue: realMaxVal)
         }
         if shouldMoveLast {
-            upperValue += deltaValue
-            upperValue = boundValue(upperValue, toLowerValue: lowerValue,
-                                       upperValue: max)
+//            upperValue += deltaValue
+            let realMinVal = valueForPosition(lowerKnob.frame.maxX + (size.width / 2))
+//            // if you chose step value such that max != min + step * [INT] that's your problem not mine
+            upperValue = boundValueStepped(valueForPosition(loc.x), toLowerValue: realMinVal, upperValue: max)
         }
         // 3
-        CATransaction.begin()
-        CATransaction.setDisableActions(true)
-        updateLayerFrames()
-        CATransaction.commit()
-        self.needsDisplay = true
         sendAction(self.action!, to: self.target)
     }
     
-    // 4
     private func boundValue(_ value: CGFloat, toLowerValue lowerValue: CGFloat, upperValue: CGFloat) -> CGFloat {
         return Swift.min(Swift.max(value, lowerValue), upperValue)
     }
     
-    // Mouse up event : We "deselect" both knobs.
-    public override func mouseUp(with theEvent: NSEvent) {
-        shouldMoveLast  = false
-        shouldMoveFirst = false
+    // 4
+    private func boundValueStepped(_ value: CGFloat, toLowerValue lowerValue: CGFloat, upperValue: CGFloat) -> CGFloat {
+        var steppedValue = getSteppedValue(value, rule: .toNearestOrAwayFromZero)
+        if (steppedValue < lowerValue) {
+            steppedValue = getSteppedValue(lowerValue, rule: .up)
+        }
+        if (steppedValue > upperValue) {
+            steppedValue = getSteppedValue(upperValue, rule: .down)
+        }
+        return steppedValue
     }
     
-    /// If has a delegate we will send changed notification, and new values for slider.
-    /// Also we trigger action if this control has one.
-//    func create() {
-//        if self.action != nil {
-//            NSApp.sendAction(self.action!, to: self.target, from: self)
-//        }
-//
-//        if let delegate = self.delegate {
-//            delegate.controller(self, didChangeFirstValue: lower, secondValue: upper)
-//        }
-//    }
-    
-    
-//    var minimValue: CGFloat = 10
-//    var delegate: RangeSliderDelegate?
-//
-//    var firstKnob: CustomKnob = CustomKnob()
-//    var secondKnob: CustomKnob = CustomKnob()
-//    var firstLabel: NSTextField = NSTextField(frame: NSMakeRect(0, -2, 30, 20))
-//    var secondLabel: NSTextField = NSTextField(frame: NSMakeRect(0, -2, 30, 20))
-//    var yOrigin: CGFloat = 0
-//    var lineMaxWidth: CGFloat = 0
+    // Mouse up event : We "deselect" both knobs.
+    public override func mouseUp(with theEvent: NSEvent) {
+        shouldMoveLast = false
+        shouldMoveFirst = false
+    }
 
-//
-//    override init(frame frameRect: NSRect) {
-//        super.init(frame: frameRect)
-//        commonInit()
-//    }
-//
-//
-//    required init?(coder: NSCoder) {
-//        super.init(coder: coder)
-//        commonInit()
-//    }
-//
-//    func commonInit() {
-//        self.wantsLayer = true
-//        self.addSubview(firstKnob)
-//        self.addSubview(secondKnob)
-//        self.addSubview(firstLabel)
-//        self.addSubview(secondLabel)
-//        setUpLabels()
-//        initViews()
-//
-//    }
-//
-//    override func draw(_ dirtyRect: NSRect) {
-//        super.draw(dirtyRect)
-//        setUpLabels()
-//        initViews()
-//    }
-//
-//    func initViews() {
-//        let textOrigin: CGFloat = 0
-//        let knobSize = self.frame.height * 0.4
-//        lineMaxWidth = self.frame.width - knobSize
-//        yOrigin = self.frame.height * 0.6
-//        let firstX = (firstValue * lineMaxWidth) /  maxValue
-//        let secondX = (secondValue * lineMaxWidth) / maxValue
-//        firstKnob.setFrameSize(NSMakeSize(knobSize, knobSize))
-//        secondKnob.setFrameSize(NSMakeSize(knobSize, knobSize))
-//        firstKnob.setFrameOrigin(NSMakePoint(firstX, yOrigin))
-//        secondKnob.setFrameOrigin(NSMakePoint(secondX, yOrigin))
-//        firstLabel.setFrameOrigin(NSMakePoint(firstX, textOrigin))
-//        secondLabel.setFrameOrigin(NSMakePoint(secondX, textOrigin))
-//
-//        firstLabel.textColor = textColor
-//        secondLabel.textColor = textColor
-//
-//        // Center text Label if is posible
-//        if firstX > 8 {
-//            firstLabel.setFrameOrigin(NSMakePoint(firstX - 8, textOrigin))
-//        }
-//        if secondX < lineMaxWidth - 8 {
-//            secondLabel.setFrameOrigin(NSMakePoint(secondX - 8, textOrigin))
-//        }
-//
-//        if secondX > lineMaxWidth - 8 {
-//            secondLabel.setFrameOrigin(NSMakePoint(lineMaxWidth - 16, textOrigin))
-//        }
-//        if (firstLabel.frame.origin.x + NSWidth(firstLabel.frame) ) > secondLabel.frame.origin.x {
-//            let size  = (secondLabel.frame.origin.x  - (firstLabel.frame.origin.x + NSWidth(firstLabel.frame) )) / 2
-//            var state = true
-//            if firstX < 8 {
-//                state = false
-//                secondLabel.setFrameOrigin(NSMakePoint(secondLabel.frame.origin.x - size - size, textOrigin))
-//            }
-//            if secondX > lineMaxWidth - 8 {
-//                state = false
-//                firstLabel.setFrameOrigin(NSMakePoint(firstLabel.frame.origin.x + size + size, textOrigin))
-//            }
-//            if state {
-//                firstLabel.setFrameOrigin(NSMakePoint(firstLabel.frame.origin.x + size, textOrigin))
-//                secondLabel.setFrameOrigin(NSMakePoint(secondLabel.frame.origin.x - size, textOrigin))
-//            }
-//        }
-//        firstLabel.stringValue  = "\(firstValue)"
-//        secondLabel.stringValue = "\(secondValue)"
-//        // Draw  background line
-//        let backgroundLine = NSBezierPath()
-//        backgroundLine.move(to: NSMakePoint(knobSize * 0.5,  self.frame.height * 0.8))
-//        backgroundLine.line(to: NSMakePoint(lineMaxWidth + knobSize * 0.5 ,  self.frame.height * 0.8))
-//        backgroundLine.lineCapStyle = NSBezierPath.LineCapStyle.round
-//        backgroundLine.lineWidth = 3
-//        backgroundLineColor.set()
-//        backgroundLine.stroke()
-//        // Draw selection  line (the line between knobs)
-//        let selectionLine = NSBezierPath()
-//        selectionLine.move(to: NSMakePoint(firstX + knobSize / 2 , self.frame.height * 0.8))
-//        selectionLine.line(to: NSMakePoint(secondX + knobSize / 2 , self.frame.height * 0.8))
-//        selectionLine.lineCapStyle = NSBezierPath.LineCapStyle.round
-//        selectionLine.lineWidth = 3
-//        selectionLineColor.setStroke()
-//        selectionLine.stroke()
-//
-//    }
-//
-//
-//    func setUpLabels() {
-//        firstLabel.isBordered       = false
-//        firstLabel.identifier       = NSUserInterfaceItemIdentifier(rawValue: "10")
-//        firstLabel.isEditable       = false
-//        firstLabel.isSelectable     = false
-//        firstLabel.stringValue      = "0"
-//        firstLabel.backgroundColor  = NSColor.white.withAlphaComponent(0)
-//        firstLabel.font             = NSFont(name: "HelveticaNeue", size: 10)
-//        firstLabel.textColor        = NSColor.gray
-//        firstLabel.alignment        = NSTextAlignment.left
-//
-//        secondLabel.isBordered      = false
-//        secondLabel.identifier      = NSUserInterfaceItemIdentifier(rawValue: "10")
-//        secondLabel.isEditable      = false
-//        secondLabel.isSelectable    = false
-//        secondLabel.stringValue     = "0"
-//        secondLabel.backgroundColor = NSColor.white.withAlphaComponent(0)
-//        secondLabel.font            = NSFont(name: "HelveticaNeue", size: 10)
-//        secondLabel.textColor       = NSColor.gray
-//        secondLabel.alignment       = NSTextAlignment.left
-//
-//    }
-    
-
-    
 }
+// Center text Label if is posible
+//if firstX > 8 {
+//    firstLabel.setFrameOrigin(NSMakePoint(firstX - 8, textOrigin))
+//}
+//if secondX < lineMaxWidth - 8 {
+//    secondLabel.setFrameOrigin(NSMakePoint(secondX - 8, textOrigin))
+//}
+//
+//if secondX > lineMaxWidth - 8 {
+//    secondLabel.setFrameOrigin(NSMakePoint(lineMaxWidth - 16, textOrigin))
+//}
+//// Prevent collisions
+//if (firstLabel.frame.origin.x + NSWidth(firstLabel.frame) ) > secondLabel.frame.origin.x {
+//    let size  = (secondLabel.frame.origin.x  - (firstLabel.frame.origin.x + NSWidth(firstLabel.frame) )) / 2
+//    var state = true
+//    if firstX < 8 {
+//        state = false
+//        secondLabel.setFrameOrigin(NSMakePoint(secondLabel.frame.origin.x - size - size, textOrigin))
+//    }
+//    if secondX > lineMaxWidth - 8 {
+//        state = false
+//        firstLabel.setFrameOrigin(NSMakePoint(firstLabel.frame.origin.x + size + size, textOrigin))
+//    }
+//    if state {
+//        firstLabel.setFrameOrigin(NSMakePoint(firstLabel.frame.origin.x + size, textOrigin))
+//        secondLabel.setFrameOrigin(NSMakePoint(secondLabel.frame.origin.x - size, textOrigin))
+//    }
+//}
+//
+//}
 
